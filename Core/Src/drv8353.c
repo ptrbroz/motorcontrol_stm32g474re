cb8dc379e43562cfb6249cb5fe4d1e3a65564e30
ptrbroz <10211738+ptrbroz@users.noreply.github.com>
2021-12-10 14:29:48 +0100

    comment out debug spi prints

--- Core/Src/drv8353.c
+++ Core/Src/drv8353.c
@@ -31,49 +31,49 @@ uint16_t drv_read_register(DRVStruct drv, int reg){
 	return drv_spi_write(&drv, (1<<15)|(reg<<11));
 }
 void drv_write_register(DRVStruct drv, int reg, int val){
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_spi_write(&drv, (reg<<11)|val);
 }
 void drv_write_DCR(DRVStruct drv, int DIS_CPUV, int DIS_GDF, int OTW_REP, int PWM_MODE, int PWM_COM, int PWM_DIR, int COAST, int BRAKE, int CLR_FLT){
 	uint16_t val = (DCR<<11) | (DIS_CPUV<<9) | (DIS_GDF<<8) | (OTW_REP<<7) | (PWM_MODE<<5) | (PWM_COM<<4) | (PWM_DIR<<3) | (COAST<<2) | (BRAKE<<1) | CLR_FLT;
 	uint16_t mask = 0x07FF;
-	printf("spi w %d, data being %d \n\r", val, (val&mask));
+	//printf("spi w %d, data being %d \n\r", val, (val&mask));
 	uint16_t retval = drv_spi_write(&drv, val);
-	printf("loop retval = %d\n\r", (retval&mask));
+	//printf("loop retval = %d\n\r", (retval&mask));
 }
 void drv_write_HSR(DRVStruct drv, int LOCK, int IDRIVEP_HS, int IDRIVEN_HS){
 	uint16_t val = (HSR<<11) | (LOCK<<8) | (IDRIVEP_HS<<4) | IDRIVEN_HS;
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_spi_write(&drv, val);
 }
 void drv_write_LSR(DRVStruct drv, int CBC, int TDRIVE, int IDRIVEP_LS, int IDRIVEN_LS){
 	uint16_t val = (LSR<<11) | (CBC<<10) | (TDRIVE<<8) | (IDRIVEP_LS<<4) | IDRIVEN_LS;
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_spi_write(&drv, val);
 }
 void drv_write_OCPCR(DRVStruct drv, int TRETRY, int DEAD_TIME, int OCP_MODE, int OCP_DEG, int VDS_LVL){
 	uint16_t val = (OCPCR<<11) | (TRETRY<<10) | (DEAD_TIME<<8) | (OCP_MODE<<6) | (OCP_DEG<<4) | VDS_LVL;
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_spi_write(&drv, val);
 }
 void drv_write_CSACR(DRVStruct drv, int CSA_FET, int VREF_DIV, int LS_REF, int CSA_GAIN, int DIS_SEN, int CSA_CAL_A, int CSA_CAL_B, int CSA_CAL_C, int SEN_LVL){
 	uint16_t val = (CSACR<<11) | (CSA_FET<<10) | (VREF_DIV<<9) | (LS_REF<<8) | (CSA_GAIN<<6) | (DIS_SEN<<5) | (CSA_CAL_A<<4) | (CSA_CAL_B<<3) | (CSA_CAL_C<<2) | SEN_LVL;
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_spi_write(&drv, val);
 }
 void drv_enable_gd(DRVStruct drv){
 	uint16_t val = (drv_read_register(drv, DCR)) & (~(0x1<<2));
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_write_register(drv, DCR, val);
 }
 void drv_disable_gd(DRVStruct drv){
 	uint16_t val = (drv_read_register(drv, DCR)) | (0x1<<2);
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_write_register(drv, DCR, val);
 }
 void drv_calibrate(DRVStruct drv){
 	uint16_t val = (0x1<<4) + (0x1<<3) + (0x1<<2);
-	printf("spi w %d \n", val);
+	//printf("spi w %d \n", val);
 	drv_write_register(drv, CSACR, val);
 }
 void drv_print_faults(DRVStruct drv){
